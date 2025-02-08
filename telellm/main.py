c371d0e19dd2de11d3b2495e2c08a93ec00153ef
Daniil <ksusonic@yandex.ru>
2025-02-09 01:37:00 +0300

    History supported

--- telellm/main.py
+++ telellm/main.py
@@ -1,38 +1,24 @@
-import os
-import asyncio
-
-from aiogram.enums import ParseMode
-from dotenv import load_dotenv
-
-from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
-from lib.ollama import Ollama
-from lib.bot import LLMManager, bot_system_router, create_message_router
+from asyncio import run
+from aiogram import Dispatcher
 from logger import init_logger
+
+from telellm.lib.bot import init_bot
+from telellm.lib.bot.commands import command_router
+from telellm.lib.bot.message import message_router
 
 
 async def main():
-    load_dotenv()
     init_logger()
-
-    token = os.getenv("TOKEN")
-    if token is None:
-        raise RuntimeError("TOKEN is not set")
-
-    bot = Bot(token=token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
-    ollama_host = os.getenv("OLLAMA_HOST")
-    ollama_port = os.getenv("OLLAMA_PORT")
-    if ollama_host is None or ollama_port is None:
-        raise RuntimeError("OLLAMA_HOST or OLLAMA_PORT is not set")
-
-    llm_manager = LLMManager(bot, Ollama(ollama_host, int(ollama_port)))
+    bot = await init_bot()
 
     dp = Dispatcher()
-    dp.include_routers(bot_system_router, create_message_router(llm_manager))
+    dp.include_routers(
+        command_router,
+        message_router,
+    )
 
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    run(main())
