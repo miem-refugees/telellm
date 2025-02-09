d05fbb07e86670b258829c961b462fd9e1c561e3
Daniil <ksusonic@yandex.ru>
2025-02-09 23:27:23 +0300

    do not load model in bot startup

--- telellm/lib/bot/__init__.py
+++ telellm/lib/bot/__init__.py
@@ -1,6 +1,1 @@
-from .bot import (
-    bot as bot,
-    set_bot_name as set_bot_name,
-    get_model_name as get_model_name,
-)
-from .ollama import llm as llm
+from .bot import bot as bot, set_bot_name as set_bot_name
--- telellm/main.py
+++ telellm/main.py
@@ -3,7 +3,7 @@ from asyncio import run
 from aiogram import Dispatcher
 from logger import init_logger
 
-from telellm.lib.bot import bot, llm, set_bot_name, get_model_name
+from telellm.lib.bot import bot, set_bot_name
 from telellm.lib.bot.callback import callback_router
 from telellm.lib.bot.commands import command_router, update_bot_commands
 from telellm.lib.bot.message import message_router
@@ -23,11 +23,6 @@ async def main():
     set_bot_name(f"@{bot_me.username}")
     await update_bot_commands()
     logger.debug("updated bot commands")
-
-    init_model_name = get_model_name()
-    logger.debug("Pulling model: {}", init_model_name)
-    await llm.pull(init_model_name)
-    logger.debug("Pulled model: {}", init_model_name)
 
     await dp.start_polling(bot)
 
