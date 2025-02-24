03bdfcf6d069d300e60df4216e2b5f64dd37d411
Daniil <ksusonic@yandex.ru>
2025-02-24 20:11:22 +0300

    minor changes

--- telellm/consumer.py
+++ telellm/consumer.py
@@ -5,13 +5,13 @@ import asyncio
 from loguru import logger
 
 from telellm.lib.ollama.ollama import Ollama
-from telellm.lib.config import OLLAMA_HOST, OLLAMA_PORT
+from telellm.lib.config import OLLAMA_HOST, OLLAMA_PORT, REDIS_HOST, REDIS_PORT
 from telellm.lib.redis import TASK_QUEUE, STATUS_KEY_PREFIX
 
 
 async def process_tasks():
     ollama = Ollama(OLLAMA_HOST, OLLAMA_PORT)
-    redis_client = redis.Redis(host="localhost", port=6379, db=0)
+    redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
 
     while True:
         _, task_data = redis_client.blpop(TASK_QUEUE)  # Блокирующее чтение из очереди
